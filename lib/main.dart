987ddfe75a7ea92c5095f9cce54bbd576cfda5da
Sanush <sanushcj@gmail.com>
2023-06-03 20:43:18 +0530

    fluter updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:first_cry_ui/home/home_page.dart';
 import 'package:first_cry_ui/theme/application_theme.dart';
 import 'package:flutter/material.dart';
-
 import 'home/mybottomnavbar.dart';
+
+
 
 void main() {
   runApp(const MyApp());
--- lib/product/widgets/pricetext.dart
+++ lib/product/widgets/pricetext.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 
-import '../../theme/theme.dart';
 
 class PriceWidget extends StatelessWidget {
   const PriceWidget({
